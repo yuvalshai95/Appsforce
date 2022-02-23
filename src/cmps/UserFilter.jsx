4c53e10b25adc854c75d9994bf5df9825781aa83
Yuval <yuvalshai95@gmail.com>
2022-02-24 00:21:55 +0200

    *Add debounce to filter text search

--- src/cmps/UserFilter.jsx
+++ src/cmps/UserFilter.jsx
@@ -19,38 +19,32 @@ export const UserFilter = () => {
 
     }
 
-    const updateSearch = (e) => setFilterBy((prevFilterBy) => ({ ...prevFilterBy, location: e.target.value }));
-    const debounceCountry = debounce(updateSearch, 1500);
+    const updateSearch = (e) => {
+        const field = e.target.name
+        const value = e.target.value
+        setFilterBy((prevFilterBy) => ({ ...prevFilterBy, [field]: value }));
+    }
+
+    const debounceInputText = debounce(updateSearch, 1000);
+
 
     return (
         <div className="user-filter-container">
             <form className="user-filter-form flex column justify-center align-center" onSubmit={evt => evt.preventDefault()}>
                 <h2 className="filter-title">Search: </h2>
 
-                <input type="text" placeholder="Search by email" value={filterBy.email}
-                    onChange={(evt) => {
-                        setFilterBy((prevFilterBy) => ({ ...prevFilterBy, email: evt.target.value }))
-                        onSetFilter()
-                    }} />
-
-
-                <input type="text" placeholder="Search by name" value={filterBy.name}
-                    onChange={(evt) => {
-                        setFilterBy((prevFilterBy) => ({ ...prevFilterBy, name: evt.target.value }))
-                        onSetFilter()
-                    }} />
-
-
-                <input type="text" placeholder="Search by country"
-                    onChange={debounceCountry}
+                <input type="text" name="email" placeholder="Search by email"
+                    onChange={debounceInputText}
                 />
-                {/* <input type="text" placeholder="Search by country" value={filterBy.location}
-                    onChange={(evt) => {
-                        setFilterBy((prevFilterBy) => ({ ...prevFilterBy, location: evt.target.value }))
-                        onSetFilter()
-                        setFilterBy((prevFilterBy) => ({ ...prevFilterBy, location: evt.target.value }))
-                        onSetFilter()
-                    }} /> */}
+
+                <input type="text" name="name" placeholder="Search by name"
+                    onChange={debounceInputText}
+                />
+
+                <input type="text" name="location" placeholder="Search by country"
+                    onChange={debounceInputText}
+                />
+
             </form>
         </div>
     )
